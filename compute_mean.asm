40b3c67eaa5c25b236143dde7a9d851c87a3f310
owen-rote <112371783+owen-rote@users.noreply.github.com>
2024-04-09 18:42:43 -0700

    Add files via upload

---
+++ compute_mean.asm
@@ -0,0 +1,118 @@
+;**************************************************************************************************************************
+;Program name: "compute_mean".  This a library function contained in a single file. The function loops through each       *
+;element of the provided array (using provided size int) and computes the mean. The mean is then returned.                *
+;Copyright (C) 2024 Owen Rotenberg.                                                                                       *
+;                                                                                                                         *
+;This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License*
+;version 3 as published by the Free Software Foundation.  This program is distributed in the hope that it will be useful, *
+;but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See *
+;the GNU General Public License for more details A copy of the GNU General Public License v3 is available here:           *
+;<https://www.gnu.org/licenses/>.                                                                                         *
+;**************************************************************************************************************************
+;
+;
+;========1=========2=========3=========4=========5=========6=========7=========8=========9=========0=========1=========2**
+;Author information
+;  Author name: Owen Rotenberg
+;
+;Status
+;  This software is not an application program, but rather it is a single function licensed for use by other applications.
+;
+;Function information
+;  Function name: compute_mean
+;  Programming language: X86 assembly in Intel syntax.
+;  Date development began:  2024-March-7
+;  Date version 1.0 finished: 2024-March-13
+;  Files of this function: compute_mean.asm
+;  System requirements: an X86 platform with nasm installed or other compatible assembler.
+;  Prototype: double compute_mean(double arr[], int arr_size).
+;
+;Purpose
+;  This function computes and returns the mean of all 64-bit floats in an array
+;
+;Translation information
+;  Assemble: nasm -f elf64 -l compute_mean.lis -o compute_mean.o compute_mean.asm
+;
+;========= Begin source code ====================================================================================
+;Declaration area
+
+global compute_mean
+extern printf
+segment .data
+
+segment .bss
+backup_storage_area resb 832
+align 64
+
+segment .text
+;********** Function **********
+compute_mean:
+
+; Backup general purpose registers (GPR's)
+push rbp
+mov  rbp,rsp
+push rdi
+push rsi
+push rdx
+push rcx
+push r8
+push r9
+push r10
+push r11
+push r12
+push r13
+push r14
+push r15
+push rbx
+pushf
+
+; Set up array
+mov r13, rdi ; r13 is the array
+mov r14, rsi ; r14 is number of elements
+cvtsi2sd xmm12, r14 ; Places the int in a float-division-compatible register
+pxor xmm11, xmm11 ; Clears xmm11
+
+mov r15, 0 ; r15 is the counter (0)
+
+;********** Program Begins **********
+beginLoop:
+
+; counter == size? Quit if true
+cmp r15, r14
+je quitLoop
+
+; Add index
+addsd xmm11, [r13+r15*8]
+
+; Increment counter
+inc r15
+
+; Restart loop
+jmp beginLoop
+
+quitLoop:
+
+; Divide by # of elements
+divsd xmm11, xmm12
+
+; Move mean into xmm0 for returning
+movsd xmm0, xmm11
+
+; Restore GPRs
+popf
+pop r15
+pop r14
+pop r13
+pop r12
+pop r11
+pop r10
+pop r9
+pop r8
+pop rsi
+pop rdi
+pop rdx
+pop rcx
+pop rbx
+pop rbp
+
+ret
---
+++ input_array.asm
@@ -0,0 +1,174 @@
+;**************************************************************************************************************************
+;Program name: "input_array".  This a library function contained in a single file. This function reads a sequence of      *
+;floats entered by the user and places each inputted value into consecutive cells of an array. The loop terminates on     *
+;<enter><ctrl+d> or upon reaching (long max) elements. The number of elements entered is also returned. Input validation  *
+;is implemented to retry upon encountering an int or string.                                                              *
+;Copyright (C) 2024 Owen Rotenberg.                                                                                       *
+;                                                                                                                         *
+;This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License*
+;version 3 as published by the Free Software Foundation.  This program is distributed in the hope that it will be useful, *
+;but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See *
+;the GNU General Public License for more details A copy of the GNU General Public License v3 is available here:           *
+;<https://www.gnu.org/licenses/>.                                                                                         *
+;**************************************************************************************************************************
+;
+;
+;========1=========2=========3=========4=========5=========6=========7=========8=========9=========0=========1=========2**
+;Author information
+;  Author name: Owen Rotenberg
+;
+;Status
+;  This software is not an application program, but rather it is a single function licensed for use by other applications.
+;
+;Function information
+;  Function name: input_array
+;  Programming language: X86 assembly in Intel syntax.
+;  Date development began:  2024-March-7
+;  Date version 1.0 finished: 2024-March-12
+;  Files of this function: input_array.asm
+;  System requirements: an X86 platform with nasm installed or other compatible assembler.
+;  Prototype: long input_array(double *W, long max).
+;
+;Purpose
+;  This function loops and reads user inputted 64-bit floats and puts them into an array.
+;
+;Translation information
+;  Assemble: nasm -f elf64 -l input_array.lis -o input_array.o input_array.asm
+;
+;========= Begin source code ====================================================================================
+;Declaration area
+
+global input_array
+
+extern isfloat ; Prof. Holliday's float verification function
+extern atof    ; String to float
+extern scanf   ; Scanning input
+extern printf  ; Displaying messages
+
+segment .data
+
+stringform db "%s", 0
+nan_disp db "The last input was invalid and not entered into the array.", 10, 0
+
+segment .bss
+backup_storage_area resb 832
+align 64
+
+segment .text
+;********** Function **********
+input_array:
+
+; Backup general purpose registers (GPR's)
+push rbp
+mov  rbp,rsp
+push rdi
+push rsi
+push rdx
+push rcx
+push r8
+push r9
+push r10
+push r11
+push r12
+push r13
+push r14
+push r15
+push rbx
+pushf
+
+; Backup other registers
+mov rax, 7
+mov rdx, 0
+xsave [backup_storage_area]
+
+; Set up array
+mov r13, rdi ; r13 is array
+mov r14, rsi ; r14 is max size
+sub rsp, 1024 ; Gives us 1kb of room for float inputs
+
+; Initialize rcx as index counter
+mov r15, 0
+
+; r13 = first element of arr
+; r14 = max size
+; r15 = counter
+
+;********** Program Begins **********
+beginLoop:
+
+cmp r15, r14
+je quitLoop
+
+;********** Read Input **********
+; Read input
+mov rax, 0
+mov rdi, stringform ; "%s"
+mov rsi, rsp
+call scanf
+
+;********** Validate Input **********
+; Check for CTRL+D
+cdqe
+cmp rax, -1
+je quitLoop
+
+; Validate recent input is valid float
+mov rax, 0
+mov rdi, rsp
+call isfloat ; rax=0 if false
+
+; Compare to 0 and jump if not float
+cmp rax, 0
+je badInput ; Jump to badInput if equal to 0
+
+;********** Populate Array **********
+; Convert string to float
+mov rax, 0
+mov rdi, rsp
+call atof
+
+; Copy number into array
+movsd [r13+r15*8], xmm0 ; r15 is counter
+inc r15
+
+; Restart loop
+jmp beginLoop
+
+badInput:
+; Print error message and re-loop
+mov rax, 0
+mov rdi, nan_disp
+call printf
+jmp beginLoop
+
+quitLoop:
+; Counter sub 1024
+add rsp, 1024
+
+;********** Finalize **********
+; Restore values to non-GPRs
+mov rax, 7
+mov rdx, 0
+fxrstor [backup_storage_area]
+
+; Move counter (r15) to rax for returning
+mov rax, r15
+
+; Restore GPRs
+popf
+pop r15
+pop r14
+pop r13
+pop r12
+pop r11
+pop r10
+pop r9
+pop r8
+pop rsi
+pop rdi
+pop rdx
+pop rcx
+pop rbx
+pop rbp
+
+ret
